df85c88c6a362514b320443d622b8b9aeacbcdd0
Navin3d <smnavin65@mail.com>
2023-01-07 23:00:47 +0530

    Gateway setuped and tested

--- Secure-Health-Gateway-Service/src/main/java/gmc/project/securehealth/SecureHealthGatewayService/SecureHealthGatewayServiceApplication.java
+++ Secure-Health-Gateway-Service/src/main/java/gmc/project/securehealth/SecureHealthGatewayService/SecureHealthGatewayServiceApplication.java
@@ -2,7 +2,9 @@ package gmc.project.securehealth.SecureHealthGatewayService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SecureHealthGatewayServiceApplication {
 
